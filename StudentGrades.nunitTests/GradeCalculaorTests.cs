3bbd15d4d093bf319a54b998345faf0c27d8508e
Ruthvik011 <160176300+Ruthvik011@users.noreply.github.com>
2024-06-17 14:07:36 -0400

    Initial commit

---
+++ StudentGrades.nunitTests/GradeCalculaorTests.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentGrades.nUnitTests
+{
+    public class GradeCalculatorTests
+    {
+        private GradeCalculator _gradeCalculator { get; set; } = null;
+
+        [SetUp]
+        public void Setup()
+        {
+            _gradeCalculator = new GradeCalculator();
+        }
+
+        [TestCase(90)]
+        [TestCase(50)]
+        [TestCase(70)]
+
+        public void GetGradeByPercentage_Test(int percentage)
+        {
+            //Assign
+            //var percentage = 99;
+
+            //Act
+            var grade = _gradeCalculator.GetGradeByPercentage(percentage);
+
+            //Assert
+            Assert.AreEqual("A", grade);
+        }
+    }
+}
---
+++ StudentGrades/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentGrades;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var gradeCalculator = new GradeCalculator();
+
+        Console.Write("Enter the percentage:");
+        var percentage = Convert.ToInt32(Console.ReadLine());
+
+        var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+        Console.WriteLine($"Student Grade : {grade}");
+    }
+}
